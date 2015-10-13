c7c366775ca926573ed5da15a10845b851a28bd7
Ralph Lange <ralph.lange@gmx.de>
2015-10-13 16:08:24 +0200

    Add user name to 1bzr2git.sh

--- 1bzr2git.sh
+++ 1bzr2git.sh
@@ -18,7 +18,7 @@ else
     cd bzr2git
 
     git remote add ${branch} bzr::lp:~epics-core/epics-base/${branch}
-    git remote add github https://github.com/epics-base/${target}.git
+    git remote add github https://epics-bot@github.com/epics-base/${target}.git
 fi
 
 git fetch ${branch}
