0be63e71d5e227df74addb3ebab08c1e4476e371
Ralph Lange <ralph.lange@gmx.de>
2015-10-13 16:39:56 +0200

    Add token

--- 1bzr2git.sh
+++ 1bzr2git.sh
@@ -18,7 +18,7 @@ else
     cd bzr2git
 
     git remote add ${branch} bzr::lp:~epics-core/epics-base/${branch}
-    git remote add github https://epics-bot@github.com/epics-base/${target}.git
+    git remote add github https://epics-bot:2b4ac3ffdbfe648faffcbbb9c4f353758fe37b82@github.com/epics-base/${target}.git
 fi
 
 git fetch ${branch}
