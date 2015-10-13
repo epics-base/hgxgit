df23ab5d46dae9c2e30055eb19015b2152bde1d3
Ralph Lange <ralph.lange@gmx.de>
2015-10-13 15:28:13 +0200

    Add 1bzr2git.sh to sync a single base branch

---
+++ 1bzr2git.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+set -e -x
+
+die() {
+  echo "$1"
+  exit 1
+}
+
+branch=${1:-"3.14"}
+target=${2:-"epics-base"}
+
+if [ -d bzr2git ]
+then
+    cd bzr2git
+else
+    git init bzr2git
+
+    cd bzr2git
+
+    git remote add ${branch} bzr::lp:~epics-core/epics-base/${branch}
+    git remote add github https://github.com/epics-base/${target}.git
+fi
+
+git fetch ${branch}
+
+git push --tags github +${branch}/master:refs/heads/${branch}
